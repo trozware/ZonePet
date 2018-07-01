f6141419744a0a99d92b6d5da7bef934a5db0063
Sarah Reichelt <sarah@troz.net>
2018-07-01 15:53:44 +1000

    v1.0.1

--- ZonePet.lua
+++ ZonePet.lua
@@ -2,6 +2,7 @@ local ZonePet_EventFrame = CreateFrame("Frame")
 ZonePet_EventFrame:RegisterEvent("PLAYER_LOGIN")
 ZonePet_EventFrame:RegisterEvent("ZONE_CHANGED")
 ZonePet_EventFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+ZonePet_EventFrame:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
 
 ZonePet_LastPetChange = 0
 
@@ -15,13 +16,21 @@ ZonePet_LastPetChange = 0
 
 ZonePet_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
-        if event == "PLAYER_LOGIN" then
-            -- data not ready immediately
-        C_Timer.After(3, 
+        if event == "PLAYER_LOGIN" or event == "ZONE_CHANGED_NEW_AREA" then
+            ZonePet_LastPetChange = 0
+
+            -- data not ready immediately but force update
+            C_Timer.After(3, 
                 function()
                     processEvent()
                 end
         )
+        elseif event == "PLAYER_MOUNT_DISPLAY_CHANGED" then
+            C_Timer.After(1, 
+                function()
+                    processMountEvent()
+                end
+            )
         else
             processEvent()
         end
@@ -40,9 +49,22 @@ function processEvent()
     end
 end
 
+function processMountEvent()
+    if IsFlying() == true then
+        ZonePet_LastPetChange = 0
+        return
+    end
+
+    if IsMounted() == false and C_PetJournal.GetSummonedPetGUID() == nil then
+        ZonePet_LastPetChange = 0
+        processEvent()
+    end
+end
+
 function summonRandomPet(zoneName)
     numPets, numOwned = C_PetJournal.GetNumPets()
     validPets = {}
+    summonedPetGUID = C_PetJournal.GetSummonedPetGUID()
 
     for n = 1, numOwned do
         petID, speciesID, owned, customName, level, favorite, isRevoked, 
@@ -56,12 +78,20 @@ function summonRandomPet(zoneName)
 
     if #validPets == 0 then
         print("|c0000FF00ZonePet: " .. "|c0000FFFFYou don't own any pets from this zone - go tame some!")
+        C_PetJournal.SummonRandomPet()
     else
         -- print("You own " .. #validPets .. " pets that live in " .. zoneName)
-        petIndex = math.random(#validPets)
-
-        name = validPets[petIndex].name
-        id = validPets[petIndex].ID
+        if #validPets == 1 then
+            petIndex = math.random(1)
+            name = validPets[1].name
+            id = validPets[1].ID
+        else
+            repeat
+                petIndex = math.random(#validPets)
+                name = validPets[petIndex].name
+                id = validPets[petIndex].ID
+            until id ~= summonedPetGUID
+        end
 
         print("|c0000FF00ZonePet: " .. "|c0000FFFFSummoning " .. name)
         C_PetJournal.SummonPetByGUID(id)
